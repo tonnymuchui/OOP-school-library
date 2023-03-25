8e3d16e3401b42ba62d60e414fd3034f5b1714c7
tonnymuchui <tonykanyingah@gmail.com>
2023-03-25 06:45:39 +0300

    Fix belong-to relationship

--- student.rb
+++ student.rb
@@ -1,10 +1,10 @@
 require_relative 'person'
 
 class Student < Person
-  def initialize(age, classroom, name: 'Unknown', parent_permission: true)
-    super(age, name: name, parent_permission: parent_permission)
+  def classroom=(classroom)
     @classroom = classroom
-  end
+    classroom.students.push(self) unless classroom.students.include?(self)
+end
 
   def play_hooky
     '¯(ツ)/¯'
